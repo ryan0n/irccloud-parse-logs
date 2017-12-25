23124215896dd93c44cc1d1b320b3f81e2bc9663
Ryan Wright <ryanw@care2team.com>
2017-12-25 15:18:45 -0800

    Added some logic to ExportMySQL

--- src/ryan0n/IrcCloudParseLogs/Export/ExportMySQL.php
+++ src/ryan0n/IrcCloudParseLogs/Export/ExportMySQL.php
@@ -3,11 +3,30 @@
 namespace ryan0n\IrcCloudParseLogs\Export;
 
 use ryan0n\IrcCloudParseLogs\Model\LogLineModel;
+use Exception;
+use mysqli;
 
 class ExportMySQL implements ExportInterface
 {
+    const DB_HOST = '';
+    const DB_PORT = '';
+    const DB_USERNAME = '';
+    const DB_PASSWORD = '';
+    const DB_DATABASE = '';
+
     public function export(LogLineModel $logLine) : bool
     {
+        try {
+            $dbConn = new mysqli(
+                self::DB_HOST,
+                self::DB_USERNAME,
+                self::DB_PASSWORD,
+                self::DB_DATABASE,
+                self::DB_PORT
+            );
+        } catch (Exception $e) {
+            throw new Exception('Could not connect to database configured in ExportMySQL.');
+        }
 
     }
 }
